26a2eaf4ee01eba74263506718e94cf9d38f736b
Carlos Bunay <cbunay@outlook.com>
2024-03-25 13:41:18 -0500

    Refactor Toggle component to only allow specific child types

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -7,9 +7,12 @@ import {Switch} from '../switch'
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  return React.Children.map(children, child =>
-    React.cloneElement(child, {on, toggle}),
-  )
+  return React.Children.map(children, child => {
+    if (allowedTypes.includes(child.type)) {
+      return React.cloneElement(child, {on, toggle})
+    }
+    return child
+  })
 }
 
 function ToggleOn({on, children}) {
@@ -24,13 +27,21 @@ function ToggleButton({on, toggle, ...props}) {
   return <Switch on={on} onClick={toggle} {...props} />
 }
 
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
+
+function MyToggleButton({on, toggle}) {
+  return on ? 'the button is on yo' : 'te button is off sooo...'
+}
+
 function App() {
   return (
     <div>
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hellos</span>
         <ToggleButton />
+        <MyToggleButton />
       </Toggle>
     </div>
   )
