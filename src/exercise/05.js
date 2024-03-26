b4ed13c2579e9607a55f532019e52c5ea79ae407
Carlos Bunay <cbunay@outlook.com>
2024-03-25 21:44:56 -0500

    Refactor toggleStateReducer to handle clickedTooMuch condition

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import {Switch} from '../switch'
+import {act} from 'react-dom/test-utils'
 
 const callAll =
   (...fns) =>
@@ -64,20 +65,10 @@ function App() {
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === 'toggle' && clickedTooMuch) {
+      return {on: state.on}
     }
+    return toggleReducer(state, action)
   }
 
   const {on, getTogglerProps, getResetterProps} = useToggle({
