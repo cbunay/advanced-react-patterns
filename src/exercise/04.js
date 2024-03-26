c4bbc4a0383b521ec7fdca0b0030352d368516be
Carlos Bunay <cbunay@outlook.com>
2024-03-25 21:17:04 -0500

    Refactor onClick event handling in useToggle function

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,6 +4,14 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
@@ -12,10 +20,7 @@ function useToggle() {
     return {
       on,
       'aria-pressed': on,
-      onClick: () => {
-        toggle()
-        onClick && onClick()
-      },
+      onClick: callAll(onClick, toggle),
       ...props,
     }
   }
